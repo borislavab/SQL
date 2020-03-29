173dc5d9edfe3944c704e4ded937664a24ce9d80
bbagaliyska <bbagaliyska@gmail.com>
2020-03-29 19:04:56 +0300

    Add subqueries problem solutions for two of the example databases

---
+++ uni_code/2.subqueries/Practice3_pc.sql
@@ -0,0 +1,87 @@
+﻿use pc;
+
+/*
+1. Напишете заявка, която извежда
+производителите на персонални компютри с
+честота на процесора поне 500 MHz.
+*/
+select distinct maker
+from product
+where exists(select code
+			 from pc
+			 where pc.model = product.model
+			 and speed >= 500);
+
+select distinct maker
+from pc
+join product
+on pc.model = product.model;
+
+/*
+2. Напишете заявка, която извежда лаптопите,
+чиято честота на CPU е по-ниска от
+честотата на който и да е персонален
+компютър.*/
+select *
+from laptop
+where speed < all (select speed
+				   from pc);
+
+/*
+3. Напишете заявка, която извежда модела на
+продукта (PC, лаптоп или принтер) с най-висока цена.
+*/
+select top 1 model
+from (select model, price
+	  from laptop
+	  union
+	  select model, price
+	  from pc
+	  union
+	  select model, price
+	  from printer) as models_and_prices
+order by price desc;
+
+/*
+4. Напишете заявка, която извежда
+производителите на цветните принтери с
+най-ниска цена.
+*/
+select maker
+from product
+where model in (select model
+				from printer
+				where color = 'y'
+				and price <= all (select price
+							  from printer
+							  where color = 'y'));
+
+/*
+5. Напишете заявка, която извежда
+производителите на тези персонални
+компютри с най-малко RAM памет, които
+имат най-бързи процесори.
+*/
+select distinct maker
+from product
+where exists(select code
+			 from pc
+			 where pc.model = product.model
+			 and pc.ram <= all (select ram from pc as pc1)
+			 and pc.speed >= all (select speed from pc as pc2 where pc2.ram = pc.ram));
+
+select *
+from pc
+order by ram asc, speed desc;
+
+select maker, model
+from product;
+
+
+
+
+
+
+
+
+
---
+++ uni_code/2.subqueries/Practice3_ships.sql
@@ -0,0 +1,14 @@
+﻿use ships;
+
+/*
+1. Напишете заявка, която извежда
+страните, чиито класове кораби са с
+най-голям брой оръдия.*/select distinct countryfrom classeswhere numguns >= all (select numguns from classes);/*2. Напишете заявка, която извежда
+имената на корабите с 16-инчови
+оръдия (bore).*/select namefrom shipswhere (select bore from classes where classes.class = ships.class) = 16;select ships.namefrom classesjoin shipson classes.class = ships.classwhere bore = 16;/*3. Напишете заявка, която извежда
+имената на битките, в които са
+участвали кораби от клас ‘Kongo’.*/select battlefrom outcomeswhere (select class from ships where name = ship) = 'Kongo';/*4. Напишете заявка, която извежда
+имената на корабите, чиито брой
+оръдия е най-голям в сравнение с
+корабите със същия калибър оръдия
+(bore).*/select namefrom shipswhere (select numguns 	   from classes	   where classes.class = ships.class) >=	   all (select numguns			from classes			where classes.bore = (select bore								  from classes as class2								  where class2.class = ships.class))order by name;/*select * from classesorder by bore, numguns desc;bore 14 -> class Tennesseebore 15 -> class Bismarck, Revengebore 16 -> class North Carolinabore 18 -> class Yamatoselect namefrom shipswhere class in ('Tennessee','Bismarck','Revenge','North Carolina','Yamato')order by name;*/
